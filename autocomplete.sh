0f18fc7df0c4b7a443009ff81d0d7b18f928e295
boertel <benjamin.oertel@gmail.com>
2016-10-27 12:25:38 -0700

    simplify resolve_vasy to allow only key: value (I ended up never using nested values) and add autocomplete script

---
+++ autocomplete.sh
@@ -0,0 +1,13 @@
+_vasy()
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="$(resolve_vasy --keys)"
+
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+
+complete -F _vasy vasy
