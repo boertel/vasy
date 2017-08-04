57654ab6d98d46a1ff8f1f4969c25325c301e842
boertel <benjamin.oertel@gmail.com>
2017-08-04 11:15:51 -0700

    execute .vasy in a dumb way

--- vasy-function.sh
+++ vasy-function.sh
@@ -2,6 +2,9 @@ function vasy {
     GOTO="$(vasy-resolve $@)"
     if [[ $? == 0 ]]; then
         builtin cd $GOTO
+        if [[ -s "./.vasy" ]]; then
+            eval "$(cat .vasy)"
+        fi
     else
         echo "$GOTO"
     fi
