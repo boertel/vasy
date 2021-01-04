98a0b7a156b67ad34293b372b75350165e786825
boertel <ben@comediadesign.com>
2021-01-04 11:11:19 -0800

    fix vasy to use zsh autocomplete

--- install.sh
+++ install.sh
@@ -7,12 +7,10 @@ if [[ -z "$PROFILE" ]]; then
 fi
 
 SRC=$(pwd)
-grep "vasy-function.sh" -- $PROFILE || echo "[[ -s \"$SRC/vasy-function.sh\" ]] && source $SRC/vasy-function.sh" >> $PROFILE
-grep "vasy-completion.sh" -- $PROFILE || echo "[[ -s \"$SRC/vasy-completion.sh\" ]] && source $SRC/vasy-completion.sh" >> $PROFILE
+grep "vasy-function" -- $PROFILE || echo "[[ -s \"$SRC/vasy-function\" ]] && source $SRC/vasy-function" >> $PROFILE
+#grep "vasy-completion.sh" -- $PROFILE || echo "[[ -s \"$SRC/vasy-completion.sh\" ]] && source $SRC/vasy-completion.sh" >> $PROFILE
 
 
-DEST=$2
-if [[ -d "${DEST}" ]]; then
-    cp ./vasy-resolve $DEST
-    chmod +x $DEST/vasy-resolve
-fi
+DEST=${2:-"/usr/local/bin"}
+cp ./vasy-resolve "$DEST"
+chmod +x "$DEST"/vasy-resolve
