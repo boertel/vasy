110038bc6bef945d564ea9d7835e1781e1ce759d
boertel <benjamin.oertel@gmail.com>
2021-10-25 10:47:21 -0700

    log everything

--- install.sh
+++ install.sh
@@ -6,9 +6,12 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 vasy_base=$(pwd)
 
 function="$vasy_base/bin/vasy-function"
+echo "function: $function"
 
 profile=${ZDOTDIR:-~}/.zshrc
+echo "profile: $profile"
 already=$(grep -nF "$function" -- $profile | sed 's/:.*//' | tr '\n' ' ')
+echo "already: $already"
 
 if [[ -z "$already" ]]; then
     echo "[ -f $function ] && source $function" >> $profile
